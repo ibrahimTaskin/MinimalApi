c7caff2aad4294716d49630feac79c0a96b0c2e9
ibrahim <ibrahim@DESKTOP-BSJFPIP>
2025-06-01 17:42:02 +0300

    Add project files.

---
+++ MinimalApi/Context/TodoDb.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinimalApi.Model;
+
+namespace MinimalApi.Context
+{
+    public class TodoDb: DbContext
+    {
+      public TodoDb(DbContextOptions<TodoDb> options) : base(options)
+        {
+        }
+        public DbSet<Todo> Todos => Set<Todo>();
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Todo>().HasKey(t => t.Id);
+        }
+    }
+}
